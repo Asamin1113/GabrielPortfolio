4db0a90173311c63c6bbf5d6e91d9fc07ff8162d
Hallerbos1113 <hallerbos.po@gmail.com>
2024-08-05 08:20:28 -0300

    modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
-  let date = new Date();
+  // let date = new Date();
   // let year = date.getFullYear();
   return (
     <Container fluid className="footer">
